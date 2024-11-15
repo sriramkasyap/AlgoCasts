3e67858f0c819b4de0952b9832de1e55321d7263
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-11-16 00:05:42 +0530

    reverse Integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//   return n >= 0
+//     ? parseInt(n.toString().split("").reverse().join(""))
+//     : parseInt(n.toString().split("").reverse().join("")) * -1;
+// }
+
+// function reverseInt(n) {
+//   let nar = Math.abs(n).toString().split("");
+//   return (
+//     nar.reduce((result, digit, index) => {
+//       return result + parseInt(digit) * Math.pow(10, index);
+//     }, 0) * (n >= 0 ? 1 : -1)
+//   );
+// }
+
+function reverseInt(n) {
+  let i = Math.abs(n);
+  let reverseN = 0;
+  while (i !== 0) {
+    reverseN = reverseN * 10 + (i % 10);
+    i = Math.floor(i / 10);
+  }
+  return reverseN * (n < 0 ? -1 : 1);
+}
 
 module.exports = reverseInt;
