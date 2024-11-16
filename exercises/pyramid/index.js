08dd7d80f21d873b6c3349fa887dc9445875a0c1
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-11-16 19:20:28 +0530

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   let midindex = Math.floor((2 * n - 1) / 2);
+
+//   for (let i = 0; i < n; i++) {
+//     let level = "";
+//     for (let j = 0; j < 2 * n - 1; j++) {
+//       if (j >= midindex - i && j <= midindex + i) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  let midindex = Math.floor((2 * n - 1) / 2);
+
+  if (level.length >= midindex - row && level.length <= midindex + row) {
+    pyramid(n, row, level + "#");
+  } else {
+    pyramid(n, row, level + " ");
+  }
+}
 
 module.exports = pyramid;
