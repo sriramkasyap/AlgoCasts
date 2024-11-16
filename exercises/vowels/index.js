2bb0ca8b9c7e98339316d432996906952a71e0cb
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-11-16 19:30:27 +0530

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,37 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   return str.split("").reduce((sum, char) => {
+//     char = char.toLowerCase();
+//     if (
+//       char === "a" ||
+//       char === "e" ||
+//       char === "i" ||
+//       char === "o" ||
+//       char === "u"
+//     ) {
+//       sum++;
+//     }
+
+//     return sum;
+//   }, 0);
+// }
+
+// function vowels(str) {
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   let count = 0;
+//   for (let vowel of vowels) {
+//     if (str.toLowerCase().includes(vowel)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+function vowels(str) {
+  return str.match(/[aeiou]/gi)?.length || 0;
+}
 
 module.exports = vowels;
