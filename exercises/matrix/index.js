642596e62c9ef9020325241d7f517a98372d487a
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-11-16 21:49:35 +0530

    spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let result = new Array(n);
+
+  for (let inc = 0; inc < n; inc++) {
+    result[inc] = new Array(n).fill(0);
+  }
+
+  let i = 1,
+    startRow = 0,
+    startColumn = 0,
+    endRow = n - 1,
+    endColumn = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    for (let j = startColumn; j <= endColumn; j++) {
+      result[startRow][j] = i;
+      i++;
+    }
+    startRow++;
+
+    for (let j = startRow; j <= endRow; j++) {
+      result[j][endColumn] = i;
+      i++;
+    }
+    endColumn--;
+
+    for (let j = endColumn; j >= startColumn; j--) {
+      result[endRow][j] = i;
+      i++;
+    }
+    endRow--;
+
+    for (let j = endRow; j >= startRow; j--) {
+      result[j][startColumn] = i;
+      i++;
+    }
+    startColumn++;
+  }
+
+  return result;
+}
 
 module.exports = matrix;
