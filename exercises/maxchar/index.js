4751ffbb3951a9fcafdcac5331b0f28e8db32b0f
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-11-16 07:34:24 +0530

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,16 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let countMap = {};
+  let maxChar = null;
+  for (let char of str) {
+    countMap[char] = 1 + (countMap[char] || 0);
+    if (!maxChar) maxChar = char;
+    if (countMap[char] > countMap[maxChar]) maxChar = char;
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
