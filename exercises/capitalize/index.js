c98f4baa6bdfa9bb5d9a74cf47204188143e1ed6
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-11-16 12:53:55 +0530

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,31 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//   let words = str.split(" ");
+
+//   return words.reduce((result, word, i) => {
+//     return (
+//       result + (i === 0 ? "" : " ") + word[0].toUpperCase() + word.slice(1)
+//     );
+//   }, "");
+// }
+
+// function capitalize(str) {
+//   let stringArray = str.split("");
+//   for (let index in stringArray) {
+//     let i = parseInt(index);
+
+//     if (i === 0 || str[i - 1] === " ") {
+//       stringArray[i] = stringArray[i].toUpperCase();
+//     }
+//   }
+
+//   return stringArray.join("");
+// }
+
+function capitalize(str) {
+  return str.replace(/(\b\w)/g, (char) => char.toUpperCase());
+}
 
 module.exports = capitalize;
