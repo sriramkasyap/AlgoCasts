8ba7b09ada9f069435a5063980ee545e360f0270
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-11-16 23:25:17 +0530

    fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,39 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  return n < 2 ? n : fib(n - 1) + fib(n - 2);
+}
+
+// function fib(n) {
+//   let series = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     series.push(series[i - 2] + series[i - 1]);
+//   }
+
+//   return series[n];
+// }
+
+// function fib(n, memo = {}) {
+//   if (n < 2) return n;
+//   if (memo[n]) return memo[n];
+
+//   memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+
+//   return memo[n];
+// }
+
+function memoize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    if (cache[args]) return cache[args];
+    cache[args] = fn.apply(this, args);
+    return cache[args];
+  };
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
--- exercises/fib/test.js
+++ exercises/fib/test.js
@@ -1,25 +1,41 @@
-const fib = require('./index');
+const fib = require("./index");
 
-test('Fib function is defined', () => {
-  expect(typeof fib).toEqual('function');
+test("Fib function is defined", () => {
+  expect(typeof fib).toEqual("function");
 });
 
-test('calculates correct fib value for 1', () => {
+test("calculates correct fib value for 1", () => {
   expect(fib(1)).toEqual(1);
 });
 
-test('calculates correct fib value for 2', () => {
+test("calculates correct fib value for 2", () => {
   expect(fib(2)).toEqual(1);
 });
 
-test('calculates correct fib value for 3', () => {
+test("calculates correct fib value for 3", () => {
   expect(fib(3)).toEqual(2);
 });
 
-test('calculates correct fib value for 4', () => {
+test("calculates correct fib value for 4", () => {
   expect(fib(4)).toEqual(3);
 });
 
-test('calculates correct fib value for 15', () => {
+test("calculates correct fib value for 15", () => {
   expect(fib(39)).toEqual(63245986);
+});
+
+test("calculates correct fib value for 52", () => {
+  expect(fib(52)).toEqual(32951280099);
+});
+
+test("calculates correct fib value for 61", () => {
+  expect(fib(61)).toEqual(2504730781961);
+});
+
+test("calculates correct fib value for 67", () => {
+  expect(fib(67)).toEqual(44945570212853);
+});
+
+test("calculates correct fib value for 73", () => {
+  expect(fib(73)).toEqual(806515533049393);
 });
