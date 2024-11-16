b552f73d2d6601bdb2abb01642f0b40e7cd79d1e
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-11-16 12:10:32 +0530

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   let a = stringA.replace(/[^\w]/, "").toLowerCase().split("").sort().join("");
+//   let b = stringB.replace(/[^\w]/, "").toLowerCase().split("").sort().join("");
+
+//   return a === b;
+// }
+
+function anagrams(stringA, stringB) {
+  let charMapA = buildCharMap(stringA);
+  let charMapB = buildCharMap(stringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) return false;
+  }
+
+  return true;
+}
+
+function buildCharMap(string) {
+  let charMap = {};
+
+  for (let char of string.replace(/[^\w]/, "").toLowerCase()) {
+    charMap[char] = 1 + (charMap[char] || 0);
+  }
+
+  return charMap;
+}
 
 module.exports = anagrams;
